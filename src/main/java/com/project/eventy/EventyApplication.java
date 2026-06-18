9114adf672c5a9561cff76844f296d81cae401e6
Shardul Kulkarni <2000167633@hexaware.com>
2026-06-18 13:21:25 +0530

    Initialized Git repo for Eventy

---
+++ src/main/java/com/project/eventy/EventyApplication.java
@@ -0,0 +1,13 @@
+package com.project.eventy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/eventy/EventyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.eventy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
